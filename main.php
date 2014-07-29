945747f17cc9746e9e7f57fa49bc58cf8a5b9788
hykw <hykw1234@gmail.com>
2014-07-30 06:50:47 +0900

    親子対応

--- main.php
+++ main.php
@@ -13,21 +13,17 @@
 
 class hykwMVC
 {
-  const ROUTE_OK = 'OK';
+  const RET_OK = 'OK';
   const ROUTE_404 = '404';
   const ROUTE_CONTROLLER_NOT_FOUND = 'controller not found';
+  const ROUTE_HELPER_NOT_FOUND = 'helper not found';
 
-  private $parent_dir;
-  private $child_dir;
+  private $dir;
   private $dir_controller;
   private $dir_model;
   private $dir_view;
   
-  /*
-    親テーマの場合、$parent_dirに自分のディレクトリを、$child_dirはFALSE
-    子テーマの場合、$parent_dirに親のディレクトリを、$child_dirに自分のディレクトリを指定する
-*/
-  function __construct($parent_dir, $child_dir = FALSE, $dir_controller = '', $dir_model = '', $dir_view = '')
+  function __construct($dir, $dir_controller = '', $dir_model = '', $dir_view = '')
   {
     if ($dir_controller == '')
       $dir_controller = 'controller';
@@ -36,30 +32,11 @@ class hykwMVC
     if ($dir_view == '')
       $dir_view = 'view';
 
-    $this->parent_dir = $parent_dir;
-    $this->child_dir = $child_dir;
+    $this->dir = $dir;
     $this->dir_controller = $dir_controller;
     $this->dir_model = $dir_model;
     $this->dir_view = $dir_view;
   }
-
-  # 子に指定ファイルが無かったら親を探す
-  public function get_requireFile($file, $subdir)
-  {
-    if ($this->child_dir != FALSE) {
-      # 子テーマ
-      $controller = sprintf('%s/%s%s.php', $this->child_dir, $subdir, $file);
-      if (file_exists($controller))
-	return $controller;
-    }
-
-    # 親テーマ
-    $controller = sprintf('%s/%s%s.php', $this->parent_dir, $subdir, $file);
-    if (file_exists($controller))
-      return $controller;
-
-    return FALSE;
-  }    
 
   public function routes($routes_top = '/top')
   {
@@ -72,12 +49,17 @@ class hykwMVC
     if ($url == FALSE)
       return self::ROUTE_404;
 
-    $controller = $this->get_requireFile($url, $this->dir_controller);
-    if ($controller == FALSE)
-      return self::ROUTE_CONTROLLER_NOT_FOUND;
+    $file = sprintf('%s%s', $this->dir_controller, $url);
+    get_template_part($file);
 
-    require_once($controller);
-    return self::ROUTE_OK;
+    return self::RET_OK;
+  }
+
+  public function callHelper($helperName, $dir_helper = 'helper')
+  {
+    $file = sprintf('%s/%s/%s', $this->dir_view, $dir_helper, $helperName);
+    get_template_part($file);
+    return self::RET_OK;
   }
 
 }
